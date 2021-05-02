762009a9f908b050e769c4f5d0f11f6dca8a14a1
Mayesamomo <48134632+Mayesamomo@users.noreply.github.com>
2021-05-02 12:31:57 +0100

    initial

---
+++ property-service/src/test/java/com/rentoshare/propertyservice/PropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentoshare.propertyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
